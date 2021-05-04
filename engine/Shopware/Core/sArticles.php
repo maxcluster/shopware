0d852951b49614df2a03749d560d3ecee39ef586
Soner Sayakci <s.sayakci@shopware.com>
2021-05-04 10:13:50 +0000

    NTR - Use native_function_invocation

--- engine/Shopware/Core/sArticles.php
+++ engine/Shopware/Core/sArticles.php
@@ -851,7 +851,7 @@ class sArticles implements \Enlight_Hook
         $getGroups = $this->db->fetchAll($sql, [(int) $groupID, $customergroup]);
         $priceMatrix = [];
 
-        if (count($getGroups)) {
+        if (\count($getGroups)) {
             foreach ($getGroups as $group) {
                 $priceMatrix[$group['discountstart']] = ['percent' => $group['discount']];
                 if (!empty($group['discount'])) {
@@ -867,7 +867,7 @@ class sArticles implements \Enlight_Hook
                 return;
             }
 
-            if (!empty($doMatrix) && count($priceMatrix) == 1) {
+            if (!empty($doMatrix) && \count($priceMatrix) == 1) {
                 return;
             }
 
@@ -991,7 +991,7 @@ class sArticles implements \Enlight_Hook
             [$fetchGroup, $article]
         );
 
-        if (count($queryCheapestPrice) > 1) {
+        if (\count($queryCheapestPrice) > 1) {
             $cheapestPrice = $queryCheapestPrice[0]['price'];
             if (empty($cheapestPrice)) {
                 // No Price for this customer-group fetch defaultprice
@@ -1009,7 +1009,7 @@ class sArticles implements \Enlight_Hook
                 ";
 
                 $queryCheapestPrice = $this->db->fetchAll($sql);
-                if (count($queryCheapestPrice) > 1) {
+                if (\count($queryCheapestPrice) > 1) {
                     $cheapestPrice = $queryCheapestPrice[0]['price'];
                 } else {
                     $cheapestPrice = 0;
@@ -1199,8 +1199,8 @@ class sArticles implements \Enlight_Hook
         $price = str_replace('.', ',', (string) $price); // Replaces points with commas
         $commaPos = strpos((string) $price, ',');
         if ($commaPos) {
-            $part = substr((string) $price, $commaPos + 1, strlen((string) $price) - $commaPos);
-            switch (strlen($part)) {
+            $part = substr((string) $price, $commaPos + 1, \strlen((string) $price) - $commaPos);
+            switch (\strlen($part)) {
                 case 1:
                     $price .= '0';
                     break;
@@ -1486,7 +1486,7 @@ class sArticles implements \Enlight_Hook
         // First we add all variant images, this images has a higher priority as the normal product images
         foreach ($variantImages as $variantImage) {
             // If the image wasn't added already, we can add the image
-            if (!in_array($variantImage['id'], $addedImages)) {
+            if (!\in_array($variantImage['id'], $addedImages)) {
                 // First we have to convert the image data, to resolve the image path and get the thumbnail configuration
                 $image = $this->getDataOfProductImage($variantImage, $articleAlbum);
 
@@ -1500,7 +1500,7 @@ class sArticles implements \Enlight_Hook
         foreach ($productImages as $productImage) {
             // Add only normal images without any configuration
             // If the image wasn't added already, we can add the image
-            if (!in_array($productImage['id'], $addedImages)) {
+            if (!\in_array($productImage['id'], $addedImages)) {
                 // First we have to convert the image data, to resolve the image path and get the thumbnail configuration
                 $image = $this->getDataOfProductImage($productImage, $articleAlbum);
 
@@ -1745,7 +1745,7 @@ class sArticles implements \Enlight_Hook
                 } else {
                     $key = $translateKey;
                 }
-                if (!empty($value) && array_key_exists($key, $data[$productId])) {
+                if (!empty($value) && \array_key_exists($key, $data[$productId])) {
                     $data[$productId][$key] = $value;
                 }
             }
@@ -1882,7 +1882,7 @@ class sArticles implements \Enlight_Hook
                         }
                     }
                 }
-                foreach (array_merge($sArticle['images'], [count($sArticle['images']) => $sArticle['image']]) as $value) {
+                foreach (array_merge($sArticle['images'], [\count($sArticle['images']) => $sArticle['image']]) as $value) {
                     if (preg_match('/(.*){(.*)}/', $value['relations'])) {
                         $configuratorImages = true;
 
@@ -1911,7 +1911,7 @@ class sArticles implements \Enlight_Hook
             }
 
             if (!empty($configuratorImages)) {
-                $sArticle['images'] = array_merge($sArticle['images'], [count($sArticle['images']) => $sArticle['image']]);
+                $sArticle['images'] = array_merge($sArticle['images'], [\count($sArticle['images']) => $sArticle['image']]);
 
                 unset($sArticle['image']);
 
@@ -1931,7 +1931,7 @@ class sArticles implements \Enlight_Hook
                     $relation = $stringParsed[1];
                     $available = explode('/', $stringParsed[2]);
 
-                    if (!@count($available)) {
+                    if (!@\count($available)) {
                         $available = [0 => $stringParsed[2]];
                     }
 
@@ -1946,13 +1946,13 @@ class sArticles implements \Enlight_Hook
                             $imageFailedCheck[] = true;
                         }
                     }
-                    if ($relation === '||' && count($imageFailedCheck) && count($imageFailedCheck) >= 1 && count($available) >= 1) { // OR combination
+                    if ($relation === '||' && \count($imageFailedCheck) && \count($imageFailedCheck) >= 1 && \count($available) >= 1) { // OR combination
                         if (!empty($debug)) {
                             echo $string . " matching combination\n";
                         }
                         $sArticle['images'][$imageKey]['relations'] = '';
                         $positions[$image['position']] = $imageKey;
-                    } elseif ($relation === '&' && count($imageFailedCheck) === count($available)) { // AND combination
+                    } elseif ($relation === '&' && \count($imageFailedCheck) === \count($available)) { // AND combination
                         $sArticle['images'][$imageKey]['relations'] = '';
                         $positions[$image['position']] = $imageKey;
                     } else {
@@ -2423,7 +2423,7 @@ class sArticles implements \Enlight_Hook
             return $condition->expandVariants();
         });
 
-        if (count($conditions) > 0) {
+        if (\count($conditions) > 0) {
             $this->config->offsetSet('forceArticleMainImageInListing', 0);
             $searchResult = $this->searchService->search($criteria, $context);
             $this->config->offsetSet('forceArticleMainImageInListing', 1);
@@ -2528,7 +2528,7 @@ class sArticles implements \Enlight_Hook
         );
 
         $isSelectionSpecified = false;
-        if (isset($data['isSelectionSpecified']) || array_key_exists('isSelectionSpecified', $data)) {
+        if (isset($data['isSelectionSpecified']) || \array_key_exists('isSelectionSpecified', $data)) {
             $isSelectionSpecified = $data['isSelectionSpecified'];
         }
 
@@ -2622,14 +2622,14 @@ class sArticles implements \Enlight_Hook
         $badWords = explode(',', $this->config->get('badwords'));
         $words = array_count_values(array_diff($words, $badWords));
         foreach (array_keys($words) as $word) {
-            if (strlen($word) < 2) {
+            if (\strlen($word) < 2) {
                 unset($words[$word]);
             }
         }
         arsort($words);
 
         return htmlspecialchars(
-            implode(', ', array_slice(array_keys($words), 0, 20)),
+            implode(', ', \array_slice(array_keys($words), 0, 20)),
             ENT_QUOTES,
             'UTF-8',
             false
